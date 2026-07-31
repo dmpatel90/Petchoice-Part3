ef903ded123cb341ce143b10c66a06a0cc403fda
Dev Patel <dmpatel90@myseneca.ca>
2026-07-31 13:20:45 -0400

    updated database.js to deploy vercel

--- config/database.js
+++ config/database.js
@@ -1,16 +1,29 @@
-require("dotenv").config();
-
 const { Sequelize } = require("sequelize");
+const pg = require("pg");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+    throw new Error("DATABASE_URL is missing. Add it to the .env file.");
+}
+
+const sequelize = new Sequelize(connectionString, {
     dialect: "postgres",
-    protocol: "postgres",
+    dialectModule: pg,
     logging: false,
+
     dialectOptions: {
         ssl: {
             require: true,
             rejectUnauthorized: false
         }
+    },
+
+    pool: {
+        max: 3,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
     }
 });
 
